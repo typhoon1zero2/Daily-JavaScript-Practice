c4e22d9bff0ded48ad30494a7fd629b28ec55ca9
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-21 17:13:39 +0900

    Added button

--- review/app.js
+++ review/app.js
@@ -52,7 +52,7 @@ const randomBtn = document.querySelector(".random-btn");
 let currentItem = 0;
 
 // load initial item
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded",  () => {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
@@ -60,3 +60,34 @@ window.addEventListener("DOMContentLoaded", function () {
   info.textContent = item.text;
 });
 
+// show person based on item
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  // show next person...use arrow function
+  nextBtn.addEventListener("click",  () => {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener("click",  () => {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener("click",  () => {
+    console.log("hello");
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
