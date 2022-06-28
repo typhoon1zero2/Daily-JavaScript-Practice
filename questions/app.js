9c7b84ca66ceab3f74735527d87930f31a6efdd4
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-28 09:57:22 +0900

    USed arrow function and selector for js

--- questions/app.js
+++ questions/app.js
@@ -1,2 +1,19 @@
 //using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll(".question");
+
+questions.forEach( (question)=> {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click",  () => {
+    // console.log(question);
+
+    questions.forEach( (item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
