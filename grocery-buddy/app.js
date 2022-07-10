424c1053f857bf98c1150342ed3b7eb2a5d1ee8c
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-10 17:20:54 +0900

    Created edit option

--- grocery-buddy/app.js
+++ grocery-buddy/app.js
@@ -8,6 +8,9 @@ const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
 
