ee55761fdc2d791991580c1f080d5bb07ec48ea2
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-10 17:26:06 +0900

    Created clear items function

--- grocery-buddy/app.js
+++ grocery-buddy/app.js
@@ -84,3 +84,17 @@ function displayAlert(text, action) {
       alert.classList.remove(`alert-${action}`);
     }, 1000);
   }
+
+  // clear items
+function clearItems() {
+    const items = document.querySelectorAll(".grocery-item");
+    if (items.length > 0) {
+      items.forEach(function (item) {
+        list.removeChild(item);
+      });
+    }
+    container.classList.remove("show-container");
+    displayAlert("empty list", "danger");
+    setBackToDefault();
+    localStorage.removeItem("list");
+  }
