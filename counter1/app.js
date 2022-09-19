afa0760bebb1493ca11e4260e934ec4b344fb6df
ptnguyen617 <ptnguyen617@yahoo.com>
2022-09-19 21:13:38 +0900

    Created js function for counter

--- counter1/app.js
+++ counter1/app.js
@@ -8,3 +8,37 @@ function getElement(selection) {
     );
   }
   
+  class Counter {
+    constructor(element, value) {
+      this.counter = element;
+      this.value = value;
+      this.resetBtn = element.querySelector('.reset');
+      this.increaseBtn = element.querySelector('.increase');
+      this.decreaseBtn = element.querySelector('.decrease');
+      this.valueDOM = element.querySelector('.value');
+      this.valueDOM.textContent = this.value;
+      // bind this to all function
+      this.increase = this.increase.bind(this);
+      this.decrease = this.decrease.bind(this);
+      this.reset = this.reset.bind(this);
+  
+      this.increaseBtn.addEventListener('click', this.increase);
+      this.decreaseBtn.addEventListener('click', this.decrease);
+      this.resetBtn.addEventListener('click', this.reset);
+    }
+    increase() {
+      this.value++;
+      this.valueDOM.textContent = this.value;
+    }
+    decrease() {
+      this.value--;
+      this.valueDOM.textContent = this.value;
+    }
+    reset() {
+      this.value = 0;
+      this.valueDOM.textContent = this.value;
+    }
+  }
+  
+  const firstCounter = new Counter(getElement('.first-counter'), 100);
+  const secondCounter = new Counter(getElement('.second-counter'), 200);
